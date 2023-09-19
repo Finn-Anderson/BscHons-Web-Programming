21de5582a7184fdb5a7f878de93ec53b0ecdb272
FereWulf <finn@toxicflames.co.uk>
2023-09-19 18:30:30 +0100

    Setup easel and added posts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 //
-// This file contains object definitions for use in the game.
+// Box2dWeb definitions for use in the game.
 //
 
 var b2Vec2 = Box2D.Common.Math.b2Vec2;
@@ -40,6 +40,21 @@ function CreateBox(density, friction, restitution, type, x, y, width, height, ob
 	return thisobj;
 }
 
+function CreateTriangle(density, friction, restitution, type, x, y, width, height, objid) {
+	var [fixDef, bodyDef] = CreateObject(density, friction, restitution, type, x, y, objid);
+
+	fixDef.shape = new b2PolygonShape;
+	fixDef.vertexCount = 3;
+	fixDef.vertices[0].Set(-(width / 2), 0.0);
+	fixDef.vertices[1].Set((width / 2), 0.0);
+	fixDef.vertices[2].Set(0.0, height);
+
+	var thisobj = world.CreateBody(bodyDef).CreateFixture(fixDef);
+	thisobj.GetBody().SetUserData({id:objid});
+
+	return thisobj;
+}
+
 function CreateCircle(density, friction, restitution, type, x, y, r, objid) {
 	var [fixDef, bodyDef] = CreateObject(density, friction, restitution, type, x, y, objid);
 
@@ -49,4 +64,35 @@ function CreateCircle(density, friction, restitution, type, x, y, r, objid) {
 	thisobj.GetBody().SetUserData({id:objid});
 
 	return thisobj;
+}
+
+//
+// Easel definitions for use in the game.
+//
+var easelCan = document.getElementsByTagName("canvas")[0];
+var easelctx = easelCan.getContext("2d");
+var stage = new createjs.Stage(easelCan);
+stage.snapPixelsEnabled = true;
+
+var manifest = [
+	{src:"background.png", id:"background"},
+	{src:"ground.png", id:"ground"},
+	{src:"football.png", id:"football"},
+	{src:"post.png", id:"post"},
+	{src:"team-red.png", id:"team-red"},
+	{src:"team-blue.png", id:"team-blue"},
+];
+
+function CreateBitmap(img, b2x, b2y) {
+	var bitmap = new createjs.Bitmap(img);
+
+	var scalex = (b2x * 2) / bitmap.image.naturalWidth;
+	var scaley = (b2y * 2) / bitmap.image.naturalHeight;
+
+	bitmap.scaleX = scalex;
+	bitmap.scaleY = scaley;
+	bitmap.regX = bitmap.image.width / 2;
+	bitmap.regY = bitmap.image.height / 2;
+
+	return bitmap;
 }
--- js/world.js
+++ js/world.js
@@ -4,41 +4,81 @@ var width = 1280;
 var height = 720;
 var scale = 30;
 
-document.getElementById("b2dcan").width = width;
-document.getElementById("b2dcan").height = height;
+document.getElementsByTagName("canvas")[0].width = width;
+document.getElementsByTagName("canvas")[0].height = height;
 
 var world = new b2World(
 	new b2Vec2(0,9.81),
 	true
 );
 
-// Debugging
-var debugDraw = new b2DebugDraw();
-debugDraw.SetSprite(document.getElementById("b2dcan").getContext("2d")
-);
-debugDraw.SetDrawScale(scale);
-debugDraw.SetFillAlpha(0.3);
-debugDraw.SetLineThickness(1.0);
-debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-world.SetDebugDraw(debugDraw);
+// Create ground and walls
+var ground = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width / 2), (height - 5), (width / 2), 5, "ground");
+var ceiling = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width / 2), 5, (width / 2), 5, "ceiling");
+var leftWall = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, 5, height, 5, height, "leftWall");
+var rightWall = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width - 5), height, 5, height, "rightWall");
 
-// Update world
-function update() {
+// Create goal posts
+var redPost = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, 100, height, 5, 200, "redPost");
+var redCrossbar = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, 100, height, 5, 200, "redCrossbar");
+console.log(redCrossbar);
+
+var bluePost = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width - 100), height, 5, 200, "bluePost");
+
+// Create dynamic objects
+var football = CreateCircle(1.0, 0.2, 0.8, b2Body.b2_dynamicBody, (width / 2 - 20), (height - 300), 20, "football");
+
+// Allow football to go through posts
+var filter = redPost.GetFilterData(); // Getting either posts or football would do.
+filter.categoryBits = 0x0002;
+filter.groupIndex = -8;
+redPost.SetFilterData(filter);
+bluePost.SetFilterData(filter);
+football.SetFilterData(filter);
+
+// Load images
+var loader = new createjs.LoadQueue(false);
+loader.addEventListener("complete", start);
+loader.loadManifest(manifest, true, "./assets/");
+
+var easelfootball;
+function start() {
+	var easelbg = CreateBitmap(loader.getResult("background"), width, height);
+	easelbg.x = 0;
+	easelbg.y = 0;
+
+	var easelground = CreateBitmap(loader.getResult("ground"), width, 5);
+	easelground.x = ground.GetBody().GetPosition().x * scale;
+	easelground.y = ground.GetBody().GetPosition().y * scale;
+
+	var easelRedPost = CreateBitmap(loader.getResult("post"), 5, 200);
+	easelRedPost.x = redPost.GetBody().GetPosition().x * scale;
+	easelRedPost.y = redPost.GetBody().GetPosition().y * scale;
+
+	var easelBluePost = CreateBitmap(loader.getResult("post"), 5, 200);
+	easelBluePost.x = bluePost.GetBody().GetPosition().x * scale;
+	easelBluePost.y = bluePost.GetBody().GetPosition().y * scale;
+
+	easelfootball = CreateBitmap(loader.getResult("football"), 20, 20);
+
+	stage.addChild(easelbg, easelground, easelRedPost, easelBluePost, easelfootball);
+
+	createjs.Ticker.framerate = 60;
+	createjs.Ticker.timingMode = createjs.Ticker.RAF;
+	createjs.Ticker.addEventListener("tick", tick);
+}
+
+function tick(e) {
 	world.Step(
 		1/60, 	// FPS
 		10,		// Velocity Iterations
 		10		// Position Iterations
 	);
-	world.DrawDebugData();
 	world.ClearForces();
-	window.requestAnimationFrame(update);
+
+	easelfootball.x = football.GetBody().GetPosition().x * scale;
+	easelfootball.y = football.GetBody().GetPosition().y * scale;
+	easelfootball.rotation = football.GetBody().GetAngle() * (180 / Math.PI);
+
+	stage.update(e);
 }
-window.requestAnimationFrame(update);
-
-// Create ground and walls
-var ground = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width / 2), (height - 5), (width / 2), 5, "ground");
-var ceiling = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width / 2), 5, (width / 2), 5, "ground");
-var leftWall = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, 5, height, 5, height, "ground");
-var rightWall = CreateBox(1.0, 0.5, 0.2, b2Body.b2_staticBody, (width - 5), height, 5, height, "ground");
-
-var football = CreateCircle(1.0, 0.2, 0.8, b2Body.b2_dynamicBody, 400, 250, 20, "football");
